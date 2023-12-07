3c62b7f5b37588a55f2871e5b8120aa56dcb9ba4
Donell <donell.gill@outlook.com>
2023-12-06 21:11:50 -0500

    generated Customer, Account and Transaction Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :customer do
+    resources :accounts do
+      resources :transactions do
+      end
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
